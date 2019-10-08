a66abfa2db205b68b89f5697875bfbdf281d1186
Siddharth Bhat <siddu.druid@gmail.com>
2019-10-08 16:50:03 +0530

    Add example of string printing

--- examples/for.f90
+++ examples/for.f90
@@ -2,12 +2,15 @@ import fputc(c: char, f: FILE): void
 import fputs(s: char[100], f: FILE): void
 import stdout: FILE
 def main(i:int) : int {
+    fputs("1D array: ",stdout);
     for i : int = 0; i <= 10; i = i + 1 {
       print(i);
       fputc(' ', stdout);
     }
     fputc('\n', stdout);
 
+    fputs("2D array: ",stdout);
+    fputc('\n', stdout);
     for i : int = 0; i <= 10; i = i + 1 {
         for j : int = 0; j <= 10; j = j + 1 {
         print(j);
